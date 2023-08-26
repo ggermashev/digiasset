360b42783b024de639166669e7763d0ad291f853
ggermashev <ggermashev@gmail.com>
2023-08-27 01:05:26 +0300

    footer

--- src/components/Footer/Footer.styled.ts
+++ src/components/Footer/Footer.styled.ts
@@ -1,6 +1,60 @@
 import styled from "styled-components";
 
 const FooterStyled = styled.div`
+  min-height: 10em;
+  background-color: #c1c5fc;
+  color: #01001e;
+  width: 80%;
+  border-radius: 20vw 20vw 0 0;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding: 2em;
+  gap: 2em;
+  position: relative;
+  
+  h1 {
+    text-align: center;
+  }
+  
+  @media(max-width: 767px) {
+    width: 100%;
+  }
 `
 
-export {FooterStyled}
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  gap: 1.5em;
+  width: 100%;
+  
+  h3 {
+    text-align: center;
+  }
+  
+  @media(max-width: 1000px) {
+    h3 {
+      width: 30%;
+      
+    }
+  }
+
+  @media(max-width: 767px) {
+    h3 {
+      width: 45%;
+    }
+  }
+
+  @media(max-width: 340px) {
+    h3 {
+      width: 100%;
+    }
+  }
+`
+
+export {FooterStyled, Row}
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import {FooterStyled} from "./Footer.styled";
+import {FooterStyled, Row} from "./Footer.styled";
 
 const Footer = () => {
     return (
         <FooterStyled>
-
+            <h1>Team <span style={{textShadow: 'black 0 0 .5em', color: 'white'}}>Axiom</span></h1>
+            <Row>
+                <h3>Зворыгин <p>Владимир</p></h3>
+                <h3>Гермашев <p>Григорий</p></h3>
+                <h3>Резник <p>Максим</p></h3>
+                <h3>Новоселов <p>Константин</p></h3>
+                <h3>Городилов <p>Дмитрий</p></h3>
+            </Row>
         </FooterStyled>
     );
 };
